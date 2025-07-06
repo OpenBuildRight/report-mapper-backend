b628c632666d976e54d41a2f2fe063d8cd89da28
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-06 14:52:38 -0500

    Initial commit

---
+++ src/test/kotlin/com/watchline5/mapreport/backend/MapReportBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.watchline5.mapreport.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapReportBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
