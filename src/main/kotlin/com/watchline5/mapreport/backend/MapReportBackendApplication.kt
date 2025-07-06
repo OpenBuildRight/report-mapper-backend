fb072a4bdf70cf60759b4473d1e98444abfea769
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-06 14:52:38 -0500

    Initial commit

---
+++ src/main/kotlin/com/watchline5/mapreport/backend/MapReportBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.watchline5.mapreport.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapReportBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapReportBackendApplication>(*args)
+}
