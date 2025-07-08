781a04da8d2d1804551a60aadfe7c8d54a3f335e
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-07 21:14:14 -0500

    changed to use mappers

--- src/main/kotlin/openbuildright/reportmapper/backend/web/controller/ObservationController.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/web/controller/ObservationController.kt
@@ -1,10 +1,10 @@
 package openbuildright.reportmapper.backend.web.controller
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import openbuildright.reportmapper.backend.service.model.GeoLocationModel
-import openbuildright.reportmapper.backend.service.model.ObservationModel
+import openbuildright.reportmapper.backend.model.GeoLocationModel
+import openbuildright.reportmapper.backend.model.ObservationModel
 import openbuildright.reportmapper.backend.service.ObservationService
-import openbuildright.reportmapper.backend.service.model.ObservationCreateModel
+import openbuildright.reportmapper.backend.model.ObservationCreateModel
 import openbuildright.reportmapper.backend.web.dto.ObservationCreateDto
 import openbuildright.reportmapper.backend.web.dto.ObservationDto
 import org.springframework.beans.factory.annotation.Autowired
