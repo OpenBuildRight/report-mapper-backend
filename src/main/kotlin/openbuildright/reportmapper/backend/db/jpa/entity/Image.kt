b95bfbf2c840d9c43514d0220f88ad55d72def6a
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-07 23:21:32 -0500

    Working out data model

--- src/main/kotlin/openbuildright/reportmapper/backend/db/jpa/entity/Image.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/db/jpa/entity/Image.kt
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import openbuildright.reportmapper.backend.model.ImageModel
 import org.springframework.data.geo.Point
 import java.time.Instant
 
@@ -12,9 +15,33 @@ class Image(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "observation_id", nullable = true)
+    val observation: Observation?,
     val key: String,
     val createdTime: Instant,
     val location: Point?
 ) {
+    fun toImageModel() : ImageModel {
+        return ImageModel(
+            id = id,
+            key = key,
+            createdTime = createdTime,
+            location = location?.let { pointToGeoLocationModel(it ) }
+        )
+    }
+
+    companion object {
+        fun fromImageModel(value: ImageModel) : Image{
+            return Image(
+                id = value.id,
+                key = value.key,
+                createdTime = value.createdTime,
+                location = value.location?.let { geoLocationModelToPoint(it) },
+                observation = null
+            )
+        }
+    }
 
 }
