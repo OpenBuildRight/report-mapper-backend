b065e8dddba25d778d7465900d520e988fce947a
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-07 21:14:14 -0500

    changed to use mappers

--- src/main/kotlin/openbuildright/reportmapper/backend/db/jpa/entity/Observation.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/db/jpa/entity/Observation.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import openbuildright.reportmapper.backend.model.GeoLocationModel
+import openbuildright.reportmapper.backend.model.ObservationModel
 import org.springframework.data.geo.Point
 import java.time.Instant
 
@@ -19,4 +21,42 @@ class Observation(
     var enabled: Boolean,
     val observationSignature: String,
     val images: List<Image>
-    ) {}
+    ) {
+    fun toObservationModel() : ObservationModel {
+        return ObservationModel(
+            id = id,
+            observationTime = observationTime,
+            createdTime = createdTime,
+            updatedTime = updatedTime,
+            location = pointToGeoLocationModel(location),
+            imageIds = listOf(),
+            observationSignature = observationSignature,
+            properties = mapOf(),
+            enabled = enabled
+
+        )
+    }
+
+    companion object {
+        fun fromObservationModel(value: ObservationModel) : Observation {
+            return Observation(
+                id = value.id,
+                observationTime = value.observationTime,
+                createdTime = value.createdTime,
+                updatedTime = value.updatedTime,
+                location = geoLocationModelToPoint(value.location),
+                images = listOf(),
+                observationSignature = value.observationSignature,
+                enabled = value.enabled
+            )
+        }
+    }
+}
+
+fun pointToGeoLocationModel(value: Point) : GeoLocationModel {
+    return GeoLocationModel(latitude = value.x, longitude = value.y)
+}
+
+fun geoLocationModelToPoint(value: GeoLocationModel) : Point {
+    return Point(value.latitude,  value.longitude)
+}
--- src/main/kotlin/openbuildright/reportmapper/backend/service/ObservationService.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/service/ObservationService.kt
@@ -1,15 +1,14 @@
 package openbuildright.reportmapper.backend.service
 
 import openbuildright.reportmapper.backend.db.jpa.entity.Observation
+import openbuildright.reportmapper.backend.db.jpa.entity.geoLocationModelToPoint
 import openbuildright.reportmapper.backend.db.jpa.repository.ObservationRepository
-import openbuildright.reportmapper.backend.service.model.GeoLocationModel
-import openbuildright.reportmapper.backend.service.model.ObservationCreateModel
-import openbuildright.reportmapper.backend.service.model.ObservationModel
+import openbuildright.reportmapper.backend.model.ObservationCreateModel
+import openbuildright.reportmapper.backend.model.ObservationModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.geo.Point
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.server.ResponseStatusException
 import java.time.Instant
 import java.util.Optional
@@ -30,28 +29,13 @@ class ObservationService(
             observationTime = observationModel.observationTime,
             createdTime = now,
             updatedTime = now,
-            location = Point(
-                observationModel.location.latitude,
-                observationModel.location.longitude
-            ),
+            location = geoLocationModelToPoint(observationModel.location),
             observationSignature = cryptoService.hmac(observationToken),
             enabled = true,
             images = listOf()
         )
         val returnedObservation = observationRepository.save(observation)
-        return ObservationModel(
-            id = returnedObservation.id,
-            observationTime = returnedObservation.observationTime,
-            createdTime = returnedObservation.createdTime,
-            updatedTime = returnedObservation.updatedTime,
-            location = GeoLocationModel(
-                returnedObservation.location.x, returnedObservation.location.y,
-            ),
-            imageIds = listOf(),
-            properties = mapOf(),
-            enabled = returnedObservation.enabled,
-            observationSignature = returnedObservation.observationSignature
-        )
+        return returnedObservation.toObservationModel()
     }
     fun updateObservation(
         id: Long, observationCreateModel: ObservationCreateModel,
@@ -59,7 +43,7 @@ class ObservationService(
         force: Boolean
         ) : ObservationModel {
         val observationResponse: Optional<Observation?> = observationRepository.findById(id)
-        if (observationResponse.isEmpty()) {
+        if (observationResponse.isEmpty) {
             throw ResponseStatusException(HttpStatus.NOT_FOUND, "Observation not found.")
         }
         val observation: Observation = observationResponse.get()
@@ -73,17 +57,7 @@ class ObservationService(
         observation.updatedTime = now
         observation.location = Point(observationCreateModel.location.latitude, observationCreateModel.location.longitude)
         val observationPutResponse: Observation = observationRepository.save(observation)
-        return ObservationModel(
-            observation.id,
-            observation.observationTime,
-            observation.createdTime,
-            observation.updatedTime,
-            GeoLocationModel(observation.location.x, observation.location.y),
-            imageIds = listOf(),
-            properties = mapOf(),
-            enabled = observation.enabled,
-            observationSignature = observation.observationSignature
-        )
+        return observationPutResponse.toObservationModel()
     }
 
 }
