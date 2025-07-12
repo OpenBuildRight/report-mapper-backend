7abffa963902399e5d31d7483ba90ddaac3d0c65
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-11 21:28:23 -0500

    Inject configuration config.

--- src/main/kotlin/openbuildright/reportmapper/backend/config/CryptoConfigModel.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/config/CryptoConfigModel.kt
@@ -1,15 +1,8 @@
 package openbuildright.reportmapper.backend.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Configuration
 
 @ConfigurationProperties(prefix="openbuildright.reportmapper.crypto")
 data class CryptoConfigModel(
     val key: String
-) {
-}
-
-@Configuration
-@EnableConfigurationProperties(CryptoConfigModel::class)
-class CryptoConfig()
+)
