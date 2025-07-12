1b3ea79855127b68559d1dd0fe1a33312a296ab1
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-11 21:27:22 -0500

    Fixed configuration.

--- src/main/kotlin/openbuildright/reportmapper/backend/service/CryptoService.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/service/CryptoService.kt
@@ -4,16 +4,20 @@ import openbuildright.reportmapper.backend.config.CryptoConfigModel
 import org.apache.commons.codec.digest.HmacAlgorithms
 import org.apache.commons.codec.digest.HmacUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Service
 
 
 @Service
+@EnableConfigurationProperties(CryptoConfigModel::class)
 class CryptoService(
+/*
     @Autowired
     val config: CryptoConfigModel
+*/
 ) {
     fun hmac(value: String) : String {
-        val hmac: String? = HmacUtils(HmacAlgorithms.HMAC_SHA_256, config.key).hmacHex(value)
+        val hmac: String? = HmacUtils(HmacAlgorithms.HMAC_SHA_256, "abd").hmacHex(value)
         if (hmac == null) {
             return ""
         }
