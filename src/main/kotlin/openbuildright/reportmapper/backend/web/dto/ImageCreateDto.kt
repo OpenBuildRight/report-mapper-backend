cd833985300e30da42e51c52261b0b642fec4b74
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-27 21:10:04 -0500

    Add image upload feature

--- src/main/kotlin/openbuildright/reportmapper/backend/web/dto/ImageCreateDto.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/web/dto/ImageCreateDto.kt
@@ -1,5 +1,9 @@
 package openbuildright.reportmapper.backend.web.dto
 
+import org.springframework.web.multipart.MultipartFile
+
 data class ImageCreateDto(
-    val location: GeoLocationDto?
+    val file: MultipartFile,
+    val latitude: Double?,
+    val longitude: Double?,
 )
