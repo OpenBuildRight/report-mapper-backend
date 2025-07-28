f41bcbfde67e119e1ee56ee60f713e486accf3ff
DustinMoriarty <dustin.moriarty@protonmail.com>
2025-07-28 00:48:35 -0500

    Added image processing

--- src/main/kotlin/openbuildright/reportmapper/backend/service/ImageService.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/service/ImageService.kt
@@ -9,6 +9,7 @@ import openbuildright.reportmapper.backend.exception.NotFoundException
 import openbuildright.reportmapper.backend.model.GeoLocationModel
 import openbuildright.reportmapper.backend.model.ImageMetadataModel
 import openbuildright.reportmapper.backend.model.ImageModel
+import openbuildright.reportmapper.backend.web.InvalidImageFile
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -17,6 +18,17 @@ import java.util.Optional
 
 @Service
 class ImageService (@param:Autowired val imageRepository: ImageRepository, @param:Autowired val imageObjectRepository: ImageObjectRepository){
+
+    fun normalizeImage(data: ByteArray, name: String) {
+        var extension : String? = null;
+        val i : Int = name.lastIndexOf('.');
+        if (i > 0) {
+            extension = name.substring(i+1);
+        } else {
+            throw InvalidImageFile("Unable to determine file type from image name ${name}.")
+        }
+
+    }
 
     fun createImage(data: ByteArray, location: GeoLocationModel?) : ImageMetadataModel {
         val objectKey: String =  Instant.now().epochSecond.toString()  + UUID.randomUUID().toString().slice(IntRange(0, 8))
