10daf126bec62e92947426ebd9175de793dd6a59
Dustin Moriarty <dustin.moriarty@protonmail.com>
2025-07-30 01:09:18 -0500

    Feature/mongodb (#1)
    Switch to MongoDB. Tired of SQL.

--- src/main/kotlin/openbuildright/reportmapper/backend/config/SecurityConfig.kt
+++ src/main/kotlin/openbuildright/reportmapper/backend/config/SecurityConfig.kt
@@ -3,13 +3,9 @@ package openbuildright.reportmapper.backend.config
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -22,7 +18,7 @@ class SecurityConfig {
         http.csrf().disable()
         http {
             authorizeHttpRequests {
-                authorize( "*", permitAll)
+                authorize("*", permitAll)
                 authorize("/**", permitAll)
             }
         }
